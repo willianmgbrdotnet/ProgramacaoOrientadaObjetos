432710e9a6da45928cee02b427f62a77423721d2
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-08-09 16:37:08 -0300

    Métodos da Classe.

--- src/javaBank/contas/Conta.java
+++ src/javaBank/contas/Conta.java
@@ -40,4 +40,34 @@ public class Conta {
         this.titular = titular;
     }
 
+    //métodos da Classe - Funções da Conta
+    public void depositar(double valor){
+        //this.saldo = saldo + valor;
+        this.saldo += valor;
+    }
+
+    public  boolean sacar(double valor){
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            System.out.println("Você NÃO tem dinheiro suficiente para sacar este Valor. Saque Negado.");
+            return false;
+        }
+    }
+
+    public boolean transferir(double valor, Conta destino){
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.depositar(valor);
+            return true;
+        } else {
+            System.out.println("Você NÃO tem dinheiro suficiente para transferir este Valor. Transferência Negada.");
+            return false;
+        }
+    }
+
+    public double mostrarSaldo(){
+        return this.saldo;
+    }
 }
